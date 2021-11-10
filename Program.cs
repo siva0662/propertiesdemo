82025c9c3346d5c4df526bcbd66c6ab0b27c0978
siva0662 <sivakrishna9970@gmail.com>
2021-11-10 13:12:17 +0530

    list of Emoployyes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-
+using System.Collections.Generic;
 
 namespace propertiesdemo
 {
@@ -10,6 +10,16 @@ namespace propertiesdemo
         {
             Employee employee = new Employee{id=10001, Ename="krishna",Salary=98000};
             Console.Write(employee);
+
+            List<Employee> employees =new List<Employee>{
+                      new Employee{id =1002,Ename="siva",Salary=69000},
+                new Employee{id=1003,Ename="karnati",Salary=96000}
+            };
+            foreach(Employee emp in employees)
+            {
+                Console.WriteLine(emp);
+            }
         }
+            
     }
 }
